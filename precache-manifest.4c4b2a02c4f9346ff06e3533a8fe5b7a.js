7ca5844c74ed38734133d972c991732b76771c73
Matt Fenner <mattjfenner@gmail.com>
2022-04-21 21:50:38 +1000

    Updates

---
+++ precache-manifest.4c4b2a02c4f9346ff06e3533a8fe5b7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5b22b7bb0ed53a9b02a",
+    "url": "/inbox-play/static/css/main.b3c38a0e.chunk.css"
+  },
+  {
+    "revision": "a5b22b7bb0ed53a9b02a",
+    "url": "/inbox-play/static/js/main.a5b22b7b.chunk.js"
+  },
+  {
+    "revision": "925dda96d98ac89208c1",
+    "url": "/inbox-play/static/js/1.925dda96.chunk.js"
+  },
+  {
+    "revision": "6d6cd2cd52d8ac54b579",
+    "url": "/inbox-play/static/js/2.6d6cd2cd.chunk.js"
+  },
+  {
+    "revision": "51abbbe0400476f3953f",
+    "url": "/inbox-play/static/js/runtime~main.51abbbe0.js"
+  },
+  {
+    "revision": "d6e5e41537b94bc1d62d487ceb050a0e",
+    "url": "/inbox-play/index.html"
+  }
+];
